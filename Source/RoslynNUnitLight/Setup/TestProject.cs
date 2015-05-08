f379d054facab9928283d046e339a7066b9b595b
Dustin Campbell <dustin@teamcampbell.org>
2015-05-08 06:40:03 -0700

    Add initial TestDocument, TestProject and TestSolution

---
+++ Source/RoslynNUnitLight/Setup/TestProject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynNUnitLight.Setup
+{
+    public class TestProject
+    {
+        public string Name { get; }
+        public string LanguageName { get; }
+
+        private readonly List<TestDocument> documents;
+
+        public TestProject(string name, string languageName)
+        {
+            this.Name = name;
+            this.LanguageName = languageName;
+
+            this.documents = new List<TestDocument>();
+        }
+
+        public TestDocument AddDocument(string name)
+        {
+            var result = new TestDocument(name);
+            this.documents.Add(result);
+            return result;
+        }
+    }
+}
---
+++ Source/RoslynNUnitLight/Setup/TestSolution.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace RoslynNUnitLight.Setup
+{
+    public class TestSolution
+    {
+        public string Name { get; }
+        private readonly List<TestProject> projects;
+
+        public TestSolution(string name)
+        {
+            this.Name = name;
+            this.projects = new List<TestProject>();
+        }
+
+        public TestProject AddProject(string name, string languageName)
+        {
+            var result = new TestProject(name, languageName);
+            this.projects.Add(result);
+            return result;
+        }
+    }
+}

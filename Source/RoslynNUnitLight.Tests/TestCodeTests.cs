b4bba7ffeda23c6cd9f32c3ae9d99bceafb4771c
Dustin Campbell <dustin@teamcampbell.org>
2015-11-05 13:00:34 -0800

    Add unit tests for TestCode

---
+++ Source/RoslynNUnitLight.Tests/TestCodeTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+
+namespace RoslynNUnitLight.Tests
+{
+    [TestFixture]
+    public class TestCodeTests
+    {
+        [Test]
+        public void PositionAtStartOfText()
+        {
+            var testCode = TestCode.Parse("$$text");
+            Assert.That(testCode.Code, Is.EqualTo("text"));
+            Assert.That(testCode.Position, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void PositionAtEndOfText()
+        {
+            var testCode = TestCode.Parse("text$$");
+            Assert.That(testCode.Code, Is.EqualTo("text"));
+            Assert.That(testCode.Position, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void PositionInMiddleOfText()
+        {
+            var testCode = TestCode.Parse("te$$xt");
+            Assert.That(testCode.Code, Is.EqualTo("text"));
+            Assert.That(testCode.Position, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void NoPosition()
+        {
+            var testCode = TestCode.Parse("text");
+            Assert.That(testCode.Code, Is.EqualTo("text"));
+            Assert.That(testCode.Position, Is.Null);
+        }
+
+        [Test]
+        public void NoPosition_1_DollarSign()
+        {
+            var testCode = TestCode.Parse("te$xt");
+            Assert.That(testCode.Code, Is.EqualTo("te$xt"));
+            Assert.That(testCode.Position, Is.Null);
+        }
+
+        [Test]
+        public void NoPosition_3_DollarSigns()
+        {
+            var testCode = TestCode.Parse("te$$$xt");
+            Assert.That(testCode.Code, Is.EqualTo("te$$$xt"));
+            Assert.That(testCode.Position, Is.Null);
+        }
+
+        [Test]
+        public void NoPosition_4_DollarSigns()
+        {
+            var testCode = TestCode.Parse("te$$$$xt");
+            Assert.That(testCode.Code, Is.EqualTo("te$$$$xt"));
+            Assert.That(testCode.Position, Is.Null);
+        }
+
+        [Test]
+        public void TwoPositionsThrows()
+        {
+            Assert.That(() => TestCode.Parse("$$text$$"), Throws.ArgumentException);
+        }
+    }
+}
